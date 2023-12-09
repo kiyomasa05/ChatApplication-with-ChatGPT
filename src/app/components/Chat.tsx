eb19737f79b0c8ea5f3f246f8937bd210e868618
kiyomasa05 <m230549@gmail.com>
2023-12-09 17:11:23 +0900

    create chat layout

--- src/app/components/Chat.tsx
+++ src/app/components/Chat.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const Chat = () => {
   return (
-    <div>Chat</div>
-  )
-}
+    <div className="bg-gray-500 h-full p-4 flex flex-col">
+      <h1 className="text-2xl text-white font-semibold mb-4">ROOM</h1>
+      <div className="flex-grow overflow-y-auto mb-4">
+        <div className="text-right">
+          <div className="bg-blue-500 inline-block rounded px-4 py-2 mb-2">
+            <p className="text-white font-medium">hello</p>
+          </div>
+        </div>
+        <div className="text-left">
+          <div className="bg-green-500 inline-block rounded px-4 py-2 mb-2">
+            <p className="text-white font-medium">hello</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex-shrink-0 relative">
+        <input
+          type="text"
+          placeholder="Send a Message"
+          className="border-2 rounded w-full pr-10 focus:outline-none p-2"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Chat
+export default Chat;
