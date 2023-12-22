e367d3730f998e48837d751f28866dcc0b03e0ce
kiyomasa05 <m230549@gmail.com>
2023-12-22 16:55:24 +0900

    fix responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,47 @@
 import Chat from "./components/Chat";
 import Sidebar from "./components/Sidebar";
 import { useAppContext } from "@/context/AppContext";
+import { useState } from "react";
+import { TfiAlignJustify } from "react-icons/tfi";
 
 export default function Home() {
   const { user } = useAppContext();
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+  const closeSidebar = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <div className="flex h-screen justify-center items-center">
-      <div className="h-full flex" style={{ width: "1280px" }}>
-        <div className="w-1/5 h-full border-r">
+    <div className="relative flex h-screen">
+      <button
+        onClick={toggleSidebar}
+        className="md:hidden fixed top-0 left-0 m-3"
+      >
+        <TfiAlignJustify size= {"45px"} />
+      </button>
+
+      {/* モバイル時には左側にサイドバーを表示 */}
+      <div
+        onClick={closeSidebar}
+        className={`md:hidden fixed inset-0 z-50 ${
+          isOpen ? "block" : "hidden"
+        }`}
+      >
+        <div className="bg-black opacity-50"></div>
+        <div className="absolute left-0 top-0 h-full w-4/5 bg-white">
           <Sidebar />
         </div>
-        <div className="w-4/5 h-full">
-          <Chat />
-        </div>
+      </div>
+
+      {/* PC時には常に表示 */}
+      <div className="hidden md:flex w-1/5">
+        <Sidebar />
+      </div>
+      <div className="flex-1">
+        <Chat />
       </div>
     </div>
   );
